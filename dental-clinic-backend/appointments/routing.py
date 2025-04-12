3dff5d3c66ea0c0623d2a3ccf60f110ae550d156
saparbayev-azizbek-01 <saparbayev.azizbek12@gmail.com>
2025-04-12 21:46:52 +0500

    empty project uploaded

--- dental-clinic-backend/appointments/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
- 
-websocket_urlpatterns = [
-    re_path(r'ws/appointments/(?P<doctor_id>\w+)/$', consumers.AppointmentConsumer.as_asgi()),
-] 
--- dental-clinic-backend/clinic/__init__.py
+++
@@ -1,7 +0,0 @@
-# Celery uchun sozlamalar
-from __future__ import absolute_import, unicode_literals
-
-# Celeryni o'rnatish
-from .celery import app as celery_app
-
-__all__ = ('celery_app',) 
